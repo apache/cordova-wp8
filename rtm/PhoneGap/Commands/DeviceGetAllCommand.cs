630af6ccb28c9030d059fb01897031061ac9a871
Matt Lacey <matt@mrlacey.co.uk>
2010-11-09 00:20:38 +0000

    Added version of sample implementation which works with RTM version of SDK

---
+++ rtm/PhoneGap/Commands/DeviceGetAllCommand.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DeviceGetAllCommand.cs">
+//     Copyright © 2010. All rights reserved.
+// </copyright>
+// <author>Matt Lacey</author>
+//-----------------------------------------------------------------------
+
+namespace PhoneGap.Commands
+{
+    public class DeviceGetAllCommand : PhoneGapCommand
+    {
+        public override void Execute(params string[] args)
+        {
+            // Most of this info is not available in the Beta but should be there in the RTM
+            this.HasCallback = true;
+            this.CallbackName = args[0];
+            this.CallbackArgs = new[]
+                                    {
+                                        "Windows Phone 7", // platform
+                                        "7.0.6414", // version - Microsoft.Devices.Environment.OSVersion.Version.ToString()
+                                        "???", // name
+                                        "???", // uuid
+                                        "0.9.X" // gap
+                                    };
+        }
+    }
+}
---
+++ rtm/PhoneGap/Commands/IAsyncCommand.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IAsyncCommand.cs">
+//     Copyright © 2010. All rights reserved.
+// </copyright>
+// <author>Matt Lacey</author>
+//-----------------------------------------------------------------------
+using System;
+
+namespace PhoneGap.Commands
+{
+    public interface IAsyncCommand
+    {
+        void SendAsyncResponse();
+
+        event EventHandler<PhoneGapCommand> OnCommandCompleted;
+    }
+}
