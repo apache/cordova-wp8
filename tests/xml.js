528d6fa1154ef5fb4f1a7c5cf93e9dbb2515628b
hermwong <herm.wong@gmail.com>
2011-08-11 06:15:33 +0800

    replaced the horrible vbs with a js

---
+++ tests/xml.js
@@ -0,0 +1,58 @@
+var DIR_PATH = "C:\\Users\\herm\\Documents\\hermwong\\phonegap-wp7\\tests\\";
+
+var strLine = "";
+
+var objFS = new ActiveXObject("Scripting.FileSystemObject");
+
+var objInputFile = objFS.OpenTextFile(DIR_PATH + "filelist.txt");
+var objOutputFile = objFS.CreateTextFile(DIR_PATH + "temp.txt", true);
+
+while (!objInputFile.AtEndOfStream)
+{
+	strLine = objInputFile.ReadLine();
+
+	strLine = strLine.replace(/\\/gi, "/");
+
+	objOutputFile.WriteLine("<FilePath Value=\"" + strLine + "\"/>");
+}
+
+objOutputFile.Close();
+objInputFile.Close();
+
+objFS.DeleteFile(DIR_PATH + "filelist.txt");
+
+objOutputFile = null;
+objInputFile = null;
+
+var objXmlNodeFile = objFS.OpenTextFile(DIR_PATH + "temp.txt");
+var objResourceFile = objFS.CreateTextFile(DIR_PATH + "GapSourceDictionary.xml", true);
+
+objResourceFile.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+objResourceFile.WriteLine("<GapSourceDictionary>");
+objResourceFile.WriteLine("<FilePath Value=\"www/index.html\"/>");
+objResourceFile.WriteLine("<FilePath Value=\"www/master.css\"/>");
+objResourceFile.WriteLine("<FilePath Value=\"www/main.js\"/>");
+
+while (!objXmlNodeFile.AtEndOfStream)
+{
+	strLine = objXmlNodeFile.ReadLine();
+
+	if (strLine.indexOf(".") > 0)
+	{
+		objResourceFile.WriteLine(strLine);
+	}
+}
+
+objResourceFile.WriteLine("</GapSourceDictionary>");
+
+objXmlNodeFile.Close();
+objResourceFile.Close();
+
+objFS.DeleteFile(DIR_PATH + "temp.txt");
+
+
+objXmlNodeFile = null;
+objResourceFile = null;
+
+objFS = null;
+
