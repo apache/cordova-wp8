33f852650a01ecee54c5971b1d7a1828aa30911e
hermwong <herm.wong@gmail.com>
2011-08-16 05:56:12 +0800

    implement device & notification apis

--- framework/PhoneGap/Commands/Device.cs
+++ framework/PhoneGap/Commands/Device.cs
@@ -16,6 +16,7 @@ namespace WP7GapClassLib.PhoneGap.Commands
     {
         public string Get()
         {
+            /*
             object id;
             string uuid = "???";
 
@@ -25,16 +26,73 @@ namespace WP7GapClassLib.PhoneGap.Commands
             {
                 uuid = id.ToString().Substring(2, 32);
             }
+            */
 
-            string platform = Environment.OSVersion.Platform.ToString();
-            string version = Environment.OSVersion.Version.ToString();
-            string name = DeviceStatus.DeviceName;
-            string phonegap = "1.0";
+            //string platform = Environment.OSVersion.Platform.ToString();
+            //string version = Environment.OSVersion.Version.ToString();
+            //string name = DeviceStatus.DeviceName;
+            //string phonegap = "1.0";
 
             return "";   
         }
 
+        public string name
+        {
+            get
+            {
+                return DeviceStatus.DeviceName;
+            }
+        }
 
+        public string phonegap
+        {
+            get
+            {
+                // TODO: should be able to dynamically read the PhoneGap version from somewhere...
+                return "1.0";
+            }
+        }
+
+        public string platform
+        {
+            get
+            {
+                return Environment.OSVersion.Platform.ToString();
+            }
+        }
+
+        public string uuid
+        {
+            get
+            {
+                return getUuid();
+            }
+        }
+
+        public string version
+        {
+            get
+            {
+                return Environment.OSVersion.Version.ToString();
+            }
+        }
+
+        private string getUuid()
+        {
+            object id;
+            UserExtendedProperties.TryGetValue("ANID", out id);
+
+            if (id != null)
+            {
+                return id.ToString().Substring(2, 32);
+            }
+            else
+            {
+                return "";
+            }
+
+      
+        }
         
     }
 }
