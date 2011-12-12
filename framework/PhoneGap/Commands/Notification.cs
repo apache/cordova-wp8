db322a7b511a92ce6d9a5c052c42e0d96148ae9d
Jesse MacFadyen <purplecabbage@gmail.com>
2011-12-12 15:52:09 -0800

    Cleanup usings

--- framework/PhoneGap/Commands/Notification.cs
+++ framework/PhoneGap/Commands/Notification.cs
@@ -8,25 +8,14 @@
  */
 
 using System;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-
 using Microsoft.Devices;
-using Microsoft.Phone.Scheduler;
 using System.Runtime.Serialization;
 using System.Threading;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
 using System.Windows.Resources;
-using Microsoft.Phone.Shell;
 using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework.Audio;
 
 namespace WP7GapClassLib.PhoneGap.Commands
 {
