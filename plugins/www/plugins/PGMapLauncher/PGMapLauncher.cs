98cb70ab853ea29b7a8aee9f1d142de8879a19f1
sgrebnov <sergei.grebnov@gmail.com>
2012-08-23 13:35:44 +0400

    plugins - refactored usage

--- plugins/www/plugins/PGMapLauncher/PGMapLauncher.cs
+++ plugins/www/plugins/PGMapLauncher/PGMapLauncher.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Tasks;
-using Microsoft.Phone.Controls;
-using System.Device.Location;
-using System.Runtime.Serialization;
+﻿using System.Device.Location;
 using System.Diagnostics;
+using System.Runtime.Serialization;
+using Microsoft.Phone.Tasks;
 
 namespace WP8CordovaClassLib.Cordova.Commands
 {
