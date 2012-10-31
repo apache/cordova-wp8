e27db67afc51faf3e0e919d5916a00676b251536
sgrebnov <sergei.grebnov@gmail.com>
2012-10-31 16:21:46 +0400

    fixes CB-1756 license issue with wp7 AudioPlayer.cs and AudioRecorder.xaml.cs

--- templates/standalone/cordovalib/Commands/AudioPlayer.cs
+++ templates/standalone/cordovalib/Commands/AudioPlayer.cs
@@ -168,7 +168,7 @@ namespace WP7CordovaClassLib.Cordova.Commands
                     this.buffer = new byte[recorder.GetSampleSizeInBytes(this.recorder.BufferDuration)];
                     this.recorder.BufferReady += new EventHandler<EventArgs>(recorderBufferReady);
                     this.memoryStream = new MemoryStream();
-                    this.WriteWavHeader(this.memoryStream, this.recorder.SampleRate);
+                    this.memoryStream.InitializeWavStream(this.recorder.SampleRate);
                     this.recorder.Start();
                     FrameworkDispatcher.Update();
                     this.SetState(PlayerState_Running);
@@ -495,7 +495,7 @@ namespace WP7CordovaClassLib.Cordova.Commands
                 return;
             }
 
-            this.UpdateWavHeader(this.memoryStream);
+            this.memoryStream.UpdateWavStream();
 
             try
             {
@@ -521,88 +521,7 @@ namespace WP7CordovaClassLib.Cordova.Commands
                 //TODO: log or do something else
                 throw;
             }
-        }
-
-
-
-        #region Wav format
-        // Original source http://damianblog.com/2011/02/07/storing-wp7-recorded-audio-as-wav-format-streams/
-
-        /// <summary>
-        /// Adds wav file format header to the stream
-        /// https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
-        /// </summary>
-        /// <param name="stream">Wav stream</param>
-        /// <param name="sampleRate">Sample Rate</param>
-        private void WriteWavHeader(Stream stream, int sampleRate)
-        {
-            const int bitsPerSample = 16;
-            const int bytesPerSample = bitsPerSample / 8;
-            var encoding = System.Text.Encoding.UTF8;
-
-            // ChunkID Contains the letters "RIFF" in ASCII form (0x52494646 big-endian form).
-            stream.Write(encoding.GetBytes("RIFF"), 0, 4);
-
-            // NOTE this will be filled in later
-            stream.Write(BitConverter.GetBytes(0), 0, 4);
-
-            // Format Contains the letters "WAVE"(0x57415645 big-endian form).
-            stream.Write(encoding.GetBytes("WAVE"), 0, 4);
-
-            // Subchunk1ID Contains the letters "fmt " (0x666d7420 big-endian form).
-            stream.Write(encoding.GetBytes("fmt "), 0, 4);
-
-            // Subchunk1Size 16 for PCM.  This is the size of therest of the Subchunk which follows this number.
-            stream.Write(BitConverter.GetBytes(16), 0, 4);
-
-            // AudioFormat PCM = 1 (i.e. Linear quantization) Values other than 1 indicate some form of compression.
-            stream.Write(BitConverter.GetBytes((short)1), 0, 2);
-
-            // NumChannels Mono = 1, Stereo = 2, etc.
-            stream.Write(BitConverter.GetBytes((short)1), 0, 2);
-
-            // SampleRate 8000, 44100, etc.
-            stream.Write(BitConverter.GetBytes(sampleRate), 0, 4);
-
-            // ByteRate =  SampleRate * NumChannels * BitsPerSample/8
-            stream.Write(BitConverter.GetBytes(sampleRate * bytesPerSample), 0, 4);
-
-            // BlockAlign NumChannels * BitsPerSample/8 The number of bytes for one sample including all channels.
-            stream.Write(BitConverter.GetBytes((short)(bytesPerSample)), 0, 2);
-
-            // BitsPerSample    8 bits = 8, 16 bits = 16, etc.
-            stream.Write(BitConverter.GetBytes((short)(bitsPerSample)), 0, 2);
-
-            // Subchunk2ID Contains the letters "data" (0x64617461 big-endian form).
-            stream.Write(encoding.GetBytes("data"), 0, 4);
-
-            // NOTE to be filled in later
-            stream.Write(BitConverter.GetBytes(0), 0, 4);
-        }
-
-        /// <summary>
-        /// Updates wav file format header
-        /// https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
-        /// </summary>
-        /// <param name="stream">Wav stream</param>
-        private void UpdateWavHeader(Stream stream)
-        {
-            if (!stream.CanSeek) throw new Exception("Can't seek stream to update wav header");
-
-            var oldPos = stream.Position;
-
-            // ChunkSize  36 + SubChunk2Size
-            stream.Seek(4, SeekOrigin.Begin);
-            stream.Write(BitConverter.GetBytes((int)stream.Length - 8), 0, 4);
-
-            // Subchunk2Size == NumSamples * NumChannels * BitsPerSample/8 This is the number of bytes in the data.
-            stream.Seek(40, SeekOrigin.Begin);
-            stream.Write(BitConverter.GetBytes((int)stream.Length - 44), 0, 4);
-
-            stream.Seek(oldPos, SeekOrigin.Begin);
-        }
-
-        #endregion
+        }    
 
         #region Xna loop
         /// <summary>
