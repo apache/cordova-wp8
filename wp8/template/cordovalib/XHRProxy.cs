1958625bf36df139cde2ba85ffca58360a3e43f8
Jesse MacFadyen <purplecabbage@gmail.com>
2013-06-24 17:23:13 -0700

    [CB-3993] oops, file was in the wrong place

--- wp8/template/cordovalib/XHRProxy.cs
+++ wp8/template/cordovalib/XHRProxy.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WPCordovaClassLib.CordovaLib
