1ef8f6a0a6431930f5c9826d3c315e99d9194371
Benn Mapes <benn.mapes@gmail.com>
2013-07-19 02:31:14 -0700

    [CB-4301] [CB-4300] 3.0.0

--- wp7/framework/Properties/AssemblyInfo.cs
+++ wp7/framework/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WPCordovaClassLib")]
-[assembly: AssemblyDescription("3.0.0rc1")]
+[assembly: AssemblyDescription("3.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Apache Cordova")]
 [assembly: AssemblyProduct("WPCordovaClassLib")]
