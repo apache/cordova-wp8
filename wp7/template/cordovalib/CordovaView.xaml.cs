2386d7ae1014d16d2affa98ac4a5927268dddde5
Jesse MacFadyen <purplecabbage@gmail.com>
2014-01-16 16:27:32 -0800

    oops, wp7 still needed some of those using's

--- wp7/template/cordovalib/CordovaView.xaml.cs
+++ wp7/template/cordovalib/CordovaView.xaml.cs
@@ -1,15 +1,15 @@
 /*
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 using Microsoft.Phone.Controls;
@@ -23,6 +23,8 @@ using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Resources;
+using System.Xml.Linq;
 using WPCordovaClassLib.Cordova;
 using WPCordovaClassLib.Cordova.JSON;
 using WPCordovaClassLib.CordovaLib;
@@ -395,7 +397,7 @@ namespace WPCordovaClassLib
             {
                 CordovaBrowser.InvokeScript("execScript", new string[] { nativeReady });
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 Debug.WriteLine("Error calling js to fire nativeReady event. Did you include cordova.js in your html script tag?");
             }
@@ -478,10 +480,10 @@ namespace WPCordovaClassLib
         {
             try
             {
-                Uri newLoc = new Uri(url,UriKind.RelativeOrAbsolute);
+                Uri newLoc = new Uri(url, UriKind.RelativeOrAbsolute);
                 CordovaBrowser.Navigate(newLoc);
             }
-            catch(Exception)
+            catch (Exception)
             {
 
             }
