8d537823c885a87311344e27ee7a7740ff5ba579
Moisés Gramary Barbosa <moises@webet.es>
2014-10-12 16:38:17 +0200

    Simple array testing serialize/deserialize.

--- testing/CordovaLib_Testing/JsonHelperTest.cs
+++ testing/CordovaLib_Testing/JsonHelperTest.cs
@@ -7,6 +7,38 @@ namespace CordovaLib_Testing
     [TestFixture]
     public class JsonHelperTest
     {
+        [Test]
+        public void SimpleJsonDeserializeTest()
+        {
+            string input = "[\"///CapturedImagesCache/WP_20141010_001.jpg\",\"http://foo.bar.net/foobar\",\"file\",\"WP_20141010_001.jpg\",\"image/jpg\",\"{}\",null,\"false\", \"1\",\"POST\",\"FileTransfer1982537557\"]";
+
+            string[] args = JsonHelper.Deserialize<string[]>(input);
+
+            Assert.AreEqual(11, args.Length);
+            Assert.AreEqual("///CapturedImagesCache/WP_20141010_001.jpg", args[0]);
+            Assert.AreEqual("http://foo.bar.net/foobar", args[1]);
+            Assert.AreEqual("file", args[2]);
+            Assert.AreEqual("WP_20141010_001.jpg", args[3]);
+            Assert.AreEqual("image/jpg", args[4]);
+            Assert.AreEqual("{}", args[5]);
+            Assert.IsNull(args[6]);
+            Assert.AreEqual("false", args[7]);
+            Assert.AreEqual("1", args[8]);
+            Assert.AreEqual("POST", args[9]);
+            Assert.AreEqual("FileTransfer1982537557", args[10]);
+        }
+
+        [Test]
+        public void SimpleJsonSerializeTest()
+        {
+            string input = "[\"///CapturedImagesCache/WP_20141010_001.jpg\",\"http://foo.bar.net/foobar\",\"file\",\"WP_20141010_001.jpg\",\"image/jpg\",\"{}\",null,\"false\",\"1\",\"POST\",\"FileTransfer1982537557\"]";
+
+            string[] args = { "///CapturedImagesCache/WP_20141010_001.jpg", "http://foo.bar.net/foobar", "file", "WP_20141010_001.jpg", "image/jpg", "{}", null, "false", "1", "POST", "FileTransfer1982537557" };
+
+            string compiled = JsonHelper.Serialize(args);
+
+            Assert.AreEqual(input, compiled);
+        }
 
         [Test]
         public void ComplexJsonDeserializeTest()
@@ -27,7 +59,7 @@ namespace CordovaLib_Testing
 
             string compiled = JsonHelper.Serialize(decompiled);
 
-            Assert.AreEqual(compiled, "{\"x-wsse\":\"UsernameToken Username=\\\"foo@bar.net\\\", PasswordDigest=\\\"ZTQXXXXXXXXXXXXXXXXXXXXXXXXXXXX0NTJlMGE2M2I0NGJiYWM4NA==\\\", Nonce=\\\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXA==\\\", Created=\\\"2014-10-11T18:48:07.232Z\\\"\"}");
+            Assert.AreEqual("{\"x-wsse\":\"UsernameToken Username=\\\"foo@bar.net\\\", PasswordDigest=\\\"ZTQXXXXXXXXXXXXXXXXXXXXXXXXXXXX0NTJlMGE2M2I0NGJiYWM4NA==\\\", Nonce=\\\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXA==\\\", Created=\\\"2014-10-11T18:48:07.232Z\\\"\"}", compiled);
         }
     }
 }
